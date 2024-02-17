379ef53e2c22eb40cb5a54ec6c9fab392e307a00
Danielle Williams <dscip@Danielles-MBP.lan>
2024-02-17 11:52:36 -0500

    mapping over puppies

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
+import { puppyList } from './data'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
   return (
     <>
-
+    {puppies.map(puppy=><p key ={puppy.id}>{puppy.name}</p>)}
     </>
   )
 }
